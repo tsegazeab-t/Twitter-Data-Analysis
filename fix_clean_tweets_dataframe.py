af68b0017b3904a1bc86007bfe0ef81bc939526e
Tsegazeab <mailtsega@gmail.com>
2021-06-21 15:54:35 +0300

    Fixed 'convert_to_datetime'

--- fix_clean_tweets_dataframe.py
+++ fix_clean_tweets_dataframe.py
@@ -29,9 +29,7 @@ class Clean_Tweets:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
+        df['created_at'] = pd.to_datetime(df['created_at'])
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
