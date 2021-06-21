a7f4282e1368e3eba26519a8ef0c839e41ba9075
Tsegazeab <mailtsega@gmail.com>
2021-06-21 16:16:15 +0300

    Added 'find_lang'

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -99,6 +99,8 @@ class TweetDfExtractor:
         
         return location
 
+    def find_lang(self)->list:
+        return self.df['lang'].to_list()
     
         
         
@@ -136,7 +138,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    _, tweet_list = read_json("./data/covid19.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
